d11177fe17f59066958da62287d1fccac8403f0a
Justin Paul <justin@jpaul.me>
2020-10-14 13:00:34 -0400

    Create linux-update-ns-record.sh

---
+++ linux-update-ns-record.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+requireddomain=mydomain.local
+
+# only execute on the primary nic
+if [ "$interface" != "eth0" ]
+then
+    return
+fi
+
+# When you have a new IP, perform nsupdate
+if [ "$reason" = BOUND ] || [ "$reason" = RENEW ] ||
+   [ "$reason" = REBIND ] || [ "$reason" = REBOOT ]
+then
+   host=`hostname`
+   nsupdatecmds=/var/tmp/nsupdatecmds
+     echo "update delete $host.$requireddomain a" > $nsupdatecmds
+     echo "update add $host.$requireddomain 3600 a $new_ip_address" >> $nsupdatecmds
+     echo "send" >> $nsupdatecmds
+
+     nsupdate $nsupdatecmds
+fi
