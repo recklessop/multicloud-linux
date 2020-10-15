abad647b4b7d46e46754476a8122c68617d214d9
Justin Paul <justin@jpaul.me>
2020-10-15 13:35:17 -0400

    Update hypervchecker.ps1

--- hypervchecker.ps1
+++ hypervchecker.ps1
@@ -1,5 +1,4 @@
 # All four returned values should match on a Hyper-V VM
 Get-WmiObject Win32_SystemEnclosure | select-object -Expandproperty serialnumber
 Get-WmiObject Win32_SystemEnclosure | select-object -Expandproperty SMBIOSAssetTag
-Get-WmiObject Win32_SystemEnclosure | select-object -Expandproperty serialnumber
 Get-WmiObject Win32_BaseBoard | select-object -ExpandProperty SerialNumber
